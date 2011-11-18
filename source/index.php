910f48598b0fcf0d3255d3b9d472d57e58e7b640
Gijs Kuijer <gijs.kuijer@gmail.com>
2011-11-18 16:26:01 +0100

    eerste php api musicbrainz

---
+++ source/index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>MusicMatcher test</title>
+</head>
+<body>
+<table>
+<?php
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+phpBrainz is a php class for querying the musicbrainz web service.
+Copyright (c) 2007 Jeff Sherlock
+
+*/
+
+require_once("phpapi/phpBrainz.class.php");
+
+//Create new phpBrainz object
+$phpBrainz = new phpBrainz();
+
+$args = array( 
+    "title"=>"Learn"
+);
+
+$releaseFilter = new phpBrainz_ReleaseFilter($args);
+$releaseResults = $phpBrainz->findRelease($releaseFilter);
+
+foreach ($releaseResults as $result) {
+	echo '<tr><td>'.$result->getTitle().'</td><td>'.$result->getArtist()->getName().'</td></tr>';
+}
+
+
+
+?>
+</table>
+</body>
+</html>
