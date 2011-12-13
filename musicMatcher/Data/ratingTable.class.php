19845bd9a302b6ca30c82e4c4098b77a229dbae8
Gijs Kuijer <gijs.kuijer@gmail.com>
2011-12-13 16:37:29 +0100

    update ratingTable

--- musicMatcher/Data/ratingTable.class.php
+++ musicMatcher/Data/ratingTable.class.php
@@ -11,7 +11,6 @@
 		
 		public function getRelatedArtist($artist,$amount) {
 			include("connect.php");
-			$artId = 0;
 			$sql = "SELECT id FROM artists WHERE name='".$artist."'";
 			
 			$result = mysql_query($sql) or die(mysql_error());
@@ -23,25 +22,26 @@
 			}
 			
 			$key = array_search($artistId, $this->artists);
-			//$key = array_search($artistId, $var);
-			$searchArray = explode("\t",$this->tableLines[$key]);
-			$searchArray[0] = -1;
-			$returnArray = array();
-			if(count($searchArray)-1 < $amount) {
-				$amount = count($searchArray)-1;
-			}
-			//$returnArray[0] = count($this->table);
 			
-			for($i = 0; $i < $amount; $i++) {
-				$max = max($searchArray);
-				$key2 = array_search($max,$searchArray);
-				$artistId = $this->artists[$key2];
-				$query = "SELECT name FROM artists WHERE id='".$artistId."'";
-				$result = mysql_query($query) or die(mysql_error());
-				while($row = mysql_fetch_array($result)){
-					$returnArray[$i] = $row['name']."\t".$max;
+			if($key != 0) {
+				$searchArray = explode("\t",$this->tableLines[$key]);
+				$searchArray[0] = -1;
+				$returnArray = array();
+				if(count($searchArray)-1 < $amount) {
+					$amount = count($searchArray)-1;
 				}
-				$searchArray[$key2] = -1;
+				
+				for($i = 0; $i < $amount; $i++) {
+					$max = max($searchArray);
+					$key2 = array_search($max,$searchArray);
+					$artistId = $this->artists[$key2];
+					$query = "SELECT name FROM artists WHERE id='".$artistId."'";
+					$result = mysql_query($query) or die(mysql_error());
+					while($row = mysql_fetch_array($result)){
+						$returnArray[$i] = $row['name']."\t".$max;
+					}
+					$searchArray[$key2] = -1;
+				}
 			}
 			
 			return $returnArray;
